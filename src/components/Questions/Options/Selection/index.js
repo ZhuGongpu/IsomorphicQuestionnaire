2c584671d1464b8aaba08d32750daae4fcb69a75
朱公朴 <zhugongpu@hotmail.com>
2016-08-29 12:27:18 +0800

    Add Selection option componnet.

---
+++ src/components/Questions/Options/Selection/index.js
@@ -0,0 +1,50 @@
+/**
+ * Single / Multiple Selection(s)
+ * Created by ZhuGongpu on 16/8/29.
+ */
+import React, {PropTypes} from "react";
+import {Radio, Checkbox} from "antd";
+const RadioGroup = Radio.Group;
+const CheckboxGroup = Checkbox.Group;
+
+class Selection extends React.Component {
+
+    onCheckChange(checkedValues) {
+        //TODO
+        console.log("Selection onCheckChange: %O", checkedValues)
+    }
+
+    onSelectChange(e) {
+        //TODO
+        console.log("Selection onSelectChange: %o", e.target.value)
+    }
+
+    render() {
+        const {currentSelection, options, selectionItemClassName, allowMultiSelection} = this.props;
+
+        return allowMultiSelection ?
+            <CheckboxGroup options={options.map(option => ({label: option.text, value: option.id}))}
+                           onChange={this.onCheckChange.bind(this)}/> :
+            <RadioGroup onChange={this.onSelectChange.bind(this)} value={currentSelection}>
+                {options.map(option =>
+                    <Radio key={option.id}
+                           className={selectionItemClassName}
+                           value={option.id}>
+                        {option.text}
+                    </Radio>
+                )}
+            </RadioGroup>
+    }
+}
+
+Selection.propTypes = {
+    currentSelection: PropTypes.any,
+    allowMultiSelection: PropTypes.bool,
+    options: PropTypes.array.shape({
+        id: PropTypes.any.isRequired,
+        text: PropTypes.string.isRequired
+    }).isRequired,
+    selectionItemClassName: PropTypes.string
+};
+
+export default Selection;
---
+++ src/components/Questions/Options/index.js
@@ -0,0 +1,6 @@
+
+import Selection from "./Selection";
+
+export {
+    Selection
+}
