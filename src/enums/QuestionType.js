305300991e2c7bfcad5acdccfd9c493a8b247d46
ZhuGongpu <zhugongpu@hotmail.com>
2016-10-03 16:29:07 +0800

    Make QuestionType value `string`

--- src/enums/QuestionType.js
+++ src/enums/QuestionType.js
@@ -5,11 +5,11 @@
  */
 
 export const QuestionType = {
-    SingleChoice: {text: "单选题", value: 1},
-    MultipleChoice: {text: "多选题", value: 2},
-    Input: {text: "填空题", value: 3},
-    Dropdown: {text: '下拉选择题', value: 4},
-    Matrix: {text: "矩阵题", value: 5}
+    SingleChoice: {text: "单选题", value: "1"},
+    MultipleChoice: {text: "多选题", value: "2"},
+    Input: {text: "填空题", value: "3"},
+    Dropdown: {text: '下拉选择题', value: "4"},
+    Matrix: {text: "矩阵题", value: "5"}
 };
 
 const QuestionTypeCollection = Object.keys(QuestionType).reduce((previousValue, currentKey) => {
